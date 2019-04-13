0463977560a0ed296712ab49e0bd6a94b7236a40
Denis Imeri <denisimeri6@gmail.com>
2019-04-13 23:10:09 +0200

    a lot of content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fas, faCompass } from '@fortawesome/free-solid-svg-icons';
 import { Meta } from '@angular/platform-browser';
+import { faTwitter, faGithub, faInstagram, faYoutube, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
 	selector: 'app-root',
@@ -51,6 +52,11 @@ export class AppComponent implements OnInit {
 	];
 
 	faCompass = faCompass;
+	faTwitter = faTwitter;
+	faInstagram = faInstagram;
+	faDiscord = faDiscord;
+	faGithub = faGithub;
+	faYoutube = faYoutube;
 	navBtn;
 	sidelinks;
 	clicked: boolean;
