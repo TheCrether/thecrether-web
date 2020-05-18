bf899ef8c86a7af2084440ccfa1ec99acb63157e
TheCrether <denisimeri6@gmail.com>
2020-05-18 23:45:58 +0200

    removed index.tsx because of error

--- components/index.tsx
+++
@@ -1,6 +0,0 @@
-export * as Container from "./Container";
-export * as Date from "./Date";
-export * as Header from "./Header";
-export * as Layout from "./Layout";
-export * as Image from "./Image";
-export * as Footer from "./Footer";
