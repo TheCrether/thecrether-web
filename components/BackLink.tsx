3939aa1064ef48e1ceaa2a6c2deeb0f9fa1de2ce
TheCrether <denisimeri6@gmail.com>
2020-08-07 21:15:56 +0200

    :lipstick: forgot to set some links to bold

--- components/BackLink.tsx
+++ components/BackLink.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function BackLink({ href, msg }: Props) {
   return (
-    <div style={{ margin: "15px 0" }}>
+    <div style={{ margin: "15px 0", fontWeight: "bold" }}>
       <Link href={href ? href : "/"}>
         <a>← {msg ? msg : "Back to home"}</a>
       </Link>
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
           </Blank>
         </div>
         <Link href="/contact">
-          <a>Contact</a>
+          <a style={{ fontWeight: "bold" }}>Contact</a>
         </Link>
       </Container>
     </footer>
