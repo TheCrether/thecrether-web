bf4f57fe6f06c2f52a68112a29792ff364b6719e
TheCrether <thecrether@gmail.com>
2021-10-27 23:19:51 +0200

    :green_heart: trying out fix for mozjpeg build issue (zlib)
    github issue for reference: https://github.com/Automattic/node-canvas/issues/1779

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const withPlugins = require("next-compose-plugins");
 const path = require("path");
 
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 const nextConf = {
   webpack: (config, options) => {
     if (config.resolve.plugins) {
